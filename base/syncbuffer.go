73774edd97a050bb42cd1f3212e0b35d9327509d
packing <563388@qq.com>
2021-11-20 12:57:26 +0800

    modify sync-buffer lock.

--- base/syncbuffer.go
+++ base/syncbuffer.go
@@ -7,18 +7,18 @@ import (
 
 type SyncBuffer struct {
 	b  bytes.Buffer
-	rw sync.Mutex
+	rw sync.RWMutex
 }
 
 func (b *SyncBuffer) Read(p []byte) (int, error) {
-	b.rw.Lock()
-	defer b.rw.Unlock()
+	b.rw.RLock()
+	defer b.rw.RUnlock()
 	return b.b.Read(p)
 }
 
 func (b *SyncBuffer) Next(n int) ([]byte, int) {
-	b.rw.Lock()
-	defer b.rw.Unlock()
+	b.rw.RLock()
+	defer b.rw.RUnlock()
 	var sn = n
 	var bl = b.b.Len()
 	if bl < n {
@@ -28,8 +28,8 @@ func (b *SyncBuffer) Next(n int) ([]byte, int) {
 }
 
 func (b *SyncBuffer) Peek(n int) ([]byte, int) {
-	b.rw.Lock()
-	defer b.rw.Unlock()
+	b.rw.RLock()
+	defer b.rw.RUnlock()
 	var sn = n
 	var bl = b.b.Len()
 	if bl < n {
@@ -51,7 +51,7 @@ func (b *SyncBuffer) Reset() {
 }
 
 func (b *SyncBuffer) Len() int {
-	b.rw.Lock()
-	defer b.rw.Unlock()
+	b.rw.RLock()
+	defer b.rw.RUnlock()
 	return b.b.Len()
 }
